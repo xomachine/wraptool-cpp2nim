620ee1640f53b20d1b8b0c9c1bdcd01bee4cf8ee
xomachine <xomachiner@gmail.com>
2016-03-26 23:18:22 +0300

    Test template improvement
    Added necessary procs import to template
    Fixed Indentation
    Changed Error message when test fails

--- test_tools.nim
+++ test_tools.nim
@@ -1,8 +1,10 @@
 
 template test*[T](code:expr, answer: T) = 
-    let ii = instantiationInfo()
-    let position = "$1($2, 1)" % [ii.filename, $ii.line]
-    let test_result = (code)
-    if test_result != answer:
-      warning("\n$3Expected: $1\n$3Got: $2" %
-        [$answer, test_result, position])
+  from strutils import `%`
+  from macros import warning
+  let ii = instantiationInfo()
+  let position = "$1($2, 1)" % [ii.filename, $ii.line]
+  let test_result = (code)
+  if test_result != answer:
+    warning("\n$3 Warning: Test failed!\n$3 Expected: $1\n$3 Got:      $2" %
+      [answer.repr, test_result.repr, position])
