5e82445f72416416bfca9cde5d445f91f38c1529
xomachine <xomachiner@gmail.com>
2016-03-27 23:47:25 +0300

    Added append proc
    which creates new State with given parameter

--- state.nim
+++ state.nim
@@ -1,4 +1,5 @@
 from cppclass import CppClass
+from macros import error
 
 type
   SourceType* = enum
@@ -13,3 +14,16 @@ type
     namespace*: string
     class*: CppClass
     source*: WrapSource
+
+proc append*(self: State, namespace: string = nil,
+  class: CppClass = nil): State =
+  result = self
+  if class != nil:
+    if result.class == nil: result.class = class
+    else: error(
+      "Class can not be appended becouse it already exists in State!")
+  if namespace != nil:
+    result.namespace =
+      if result.namespace != nil:
+        result.namespace & "::" & namespace
+      else: namespace
