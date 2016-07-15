6c0f707aa157c5cf4f6ee43582607ec5053640d4
xomachine <xomachiner@gmail.com>
2016-07-15 16:20:09 +0300

    Changed model of check and require templates for better fail messages

--- tests/testtool.nim
+++ tests/testtool.nim
@@ -4,9 +4,6 @@ from terminal import resetAttributes, ForegroundColor, eraseLine
 from strutils import `%`
 
 type
-  Status* {.pure.} = enum
-    ok, failed, fatal, unknown
-
   TestFailed = object of Exception
   TestCrushed = object of Exception
 
@@ -23,7 +20,7 @@ proc colored_echo(text: string, color: ForegroundColor) {.compileTime.} =
 template internal_suite(name: string, code: untyped) {.dirty.} =
   var errors: int = 0
   var oks: int = 0
-  var teststatus: Status
+  var is_checked: bool
   echo "Started test suite \"" & name & "\""
   (code)
   echo "Test suite \"" & name & "\" completed with " & $errors &
@@ -39,38 +36,39 @@ template test*(name: string, code: untyped)  =
   echo "Testing $1 ..." % name
   #cursorUp()
   #cursorForward(notification.len)
-  teststatus = Status.unknown
+  is_checked = false
   try:
     (code)
+    if is_checked:
+      colored_echo("[  OK  ]", fgGreen)
+      oks.inc
+    else:
+      #eraseLine()
+      warning("""You have not written test for $1!
+Consider adding "check" or "require" procedure under test statement""" % name)
   except TestFailed:
-    teststatus = Status.failed
-  except TestCrushed:
-    teststatus = Status.fatal
-  case teststatus
-  of Status.ok:
-    colored_echo("[  OK  ]", fgGreen)
-    oks.inc
-  of Status.failed:
     colored_echo("[ FAIL ]", fgYellow)
     errors.inc
-  of Status.fatal:
+    echo getCurrentExceptionMsg()
+  except TestCrushed:
     colored_echo("[ FAIL ]", fgRed)
+    echo getCurrentExceptionMsg()
     error("Failed to complete tests due to critical test failure!")
-  of Status.unknown:
-    #eraseLine()
-    warning("""You have not written test for $1!
-Consider adding "check" or "require" procedure under test statement""" % name)
 
 
-template check*(cond: bool) =
-  teststatus = Status.ok
-  if not cond:
-    raise newException(TestFailed, "")
+template check(ex1: untyped, ex2: untyped, ex: typedesc) =
+  is_checked = true
+  let result1 = (ex1)
+  let result2 = (ex2)
+  if result1 != result2:
+    raise newException(ex, "$1 == $2" % [result1.repr, result2.repr])
 
-template require*(cond: bool) =
-  teststatus = Status.ok
-  if not cond:
-    raise newException(TestCrushed, "")
+template check*(ex1: untyped, ex2: untyped) =
+  check(ex1, ex2, TestFailed)
+
+template require*(ex1: untyped, ex2: untyped) =
+  check(ex1, ex2, TestCrushed)
+
 
 when isMainModule:
   static:
@@ -78,11 +76,11 @@ when isMainModule:
       test "abscence of \"check\" or \"require\". This test should throw warning":
         assert(true)
       test "checking mechanism":
-        check(true)
+        check(true, true)
       test "check failure. This test should fail":
-        check(false)
+        check(false, true)
       test "require mechanism":
-        require(true)
+        require(true, true)
     suite "Multiple suites":
       test "another test in next suite":
-        check(true)
+        check(true, true)
