8882cfe4a77e798615f84c65ac11635b79a0befc
xomachine <xomachiner@gmail.com>
2016-07-23 15:55:30 +0300

    Added tests for variable handling

--- tests/variable.nim
+++ tests/variable.nim
@@ -10,3 +10,31 @@ static:
       check(d.cppname, "a")
       check(d.typename, "cint")
       require(d.namespace.len, 0)
+
+    test "C++ name declaration":
+      let d = newVariable(parseExpr(""""_a" as a: cint"""))
+      check(d.name, "a")
+      check(d.cppname, "_a")
+      check(d.typename, "cint")
+      require(d.namespace.len, 0)
+
+    test "Namespaced declaration":
+      let d = newVariable(parseExpr("""a: cint"""), "somenamespace")
+      check(d.name, "a")
+      check(d.cppname, "a")
+      check(d.typename, "cint")
+      require(d.namespace, "somenamespace")
+
+  suite "Code generation":
+    test "Simple declaration":
+      let d = newVariable(parseExpr("a: cint")).generate_declaration()
+      check(d.repr, "a* {.importcpp: \"a\".}: cint")
+
+    test "C++ name declaration":
+      let d = newVariable(parseExpr(""""_a" as a: cint""")).generate_declaration()
+      check(d.repr, "a* {.importcpp: \"_a\".}: cint")
+
+    test "Namespaced declaration":
+      let d = newVariable(parseExpr("""a: cint"""), "somenamespace")
+        .generate_declaration()
+      check(d.repr, "a* {.importcpp: \"somenamespace::a\".}: cint")
